3e355dbd516f1008bd3b579e6f04741092e217aa
manasiPandey21 <manasiPandey92@gmail.com>
2023-02-01 00:37:32 +0530

    first commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flash_chat_flutterx/screens/login_screen.dart';
+import 'package:flash_chat_flutterx/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat_flutterx/RoundedButton/RoundedButttons.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id='welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+  @override
+  void initState() {
+    super.initState();
+    controller=AnimationController(
+
+      duration:Duration(seconds: 30),
+      upperBound: 1,
+      vsync: this,);
+    animation=CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    controller.forward();
+    // animation.addStatusListener((status) {
+    //   if(status==AnimationStatus.completed){
+    //     controller.reverse(from: 1);
+    //   }else if(status==AnimationStatus.dismissed){
+    //     controller.forward();
+    //   }
+    // });
+    animation=ColorTween(begin: Colors.pink,end: Colors.white).animate((controller));
+
+
+    controller.addListener(() {
+      setState(() {
+
+      });
+      print(animation.value);
+    });
+    @override
+    void dispose(){
+      controller.dispose();
+      super.dispose();
+    }
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text:['Flash chat'],
+                 // '${controller.value.toInt()}%',
+
+                  textStyle: TextStyle(
+                    fontSize: 55.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.black
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+              title: 'Log In',
+              colour: Colors.lightBlueAccent,
+              onPressed: (){
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+             RoundedButton(
+              title: 'Register',
+              colour: Colors.blueAccent,
+              onPressed: (){
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
